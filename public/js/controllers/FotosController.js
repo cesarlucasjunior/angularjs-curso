ce92e95ba5b4308cf164269f7b57a9bbfcaa5f73
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-05-25 14:08:29 -0300

    Busca de dados via REST e uso da ng-repeat para iterar lista

--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -1,7 +1,25 @@
-angular.module('alurapic').controller('FotosController', function($scope){
+angular.module('alurapic').controller('FotosController', function($scope, $http){
 
-    $scope.foto = {
-        titulo : 'Leão',
-        url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-    };
+    $scope.fotos = [];
+
+    $http.get('v1/fotos')
+        .success(function(fotos){
+            console.log(fotos);
+            $scope.fotos = fotos;
+        })
+        .error(function(error){
+            console.log(error);
+        })
+
+
+
+
+    // Usando recuperação de dados com ajax via $http e promisse.
+    // var promise = $http.get('v1/fotos');
+
+    // promise.then(function(retorno){
+    //     $scope.fotos = retorno.data;
+    // }).catch(function(error){
+    //     console.log(error)
+    // })
 });
