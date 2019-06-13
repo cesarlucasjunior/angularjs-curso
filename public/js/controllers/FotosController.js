7c39823f646367f526c5ac65c4ec7e0ded90c847
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-06-12 21:26:06 -0300

    Implantação de filtro e aplicação de animação via CSS

--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -1,7 +1,7 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http){
 
     $scope.fotos = [];
-    $scope.filtro = '';
+    $scope.filtro = ''; //Texto digitado pelo usuário - ng-model - two way data binding.
 
     $http.get('v1/fotos')
         .success(function(fotos){
