3e7501ba6e0974d78c9fe3911a6c376e1a5132e1
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-06-17 22:59:34 -0300

    Criando o locationProvider

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
-.config(function($routeProvider){
+.config(function($routeProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true);
 
     $routeProvider.when('/fotos', {
         templateUrl : 'partials/principal.html',
