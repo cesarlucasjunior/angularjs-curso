5ff5bfa95ab6b411251e8859f1f9d3778e743aaa
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-06-18 21:27:45 -0300

    Criação de form para inclusao de foto

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,15 @@ angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
 
     $locationProvider.html5Mode(true);
 
+    //Roteamento interno, ou seja, do angular pra gerenciarmos as views.
     $routeProvider.when('/fotos', {
         templateUrl : 'partials/principal.html',
         controller: 'FotosController'
+    });
+
+    $routeProvider.when('/fotos/new', {
+        templateUrl : 'partials/foto.html',
+        controller: 'FotoController'
     });
 
     $routeProvider.otherwise({redirectTo: '/fotos'});
