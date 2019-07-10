777f64b88623e4be00c4423c9a0dc7614e623f86
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-07-09 23:17:16 -0300

    Incluindo mensagem de resposta submit

--- public/js/controllers/FotoController.js
+++ public/js/controllers/FotoController.js
@@ -2,6 +2,7 @@ angular.module('alurapic').controller('FotoController', function($scope, $http){
 
     //Controller associada a foto.html e receberá os dados da página e realizará o processamento necessário.
     $scope.foto = {};
+    $scope.mensagem = "";
 
     //Método do formulário para enviar dados à API.
     $scope.submeter = function() {
@@ -9,8 +10,9 @@ angular.module('alurapic').controller('FotoController', function($scope, $http){
             $http.post('v1/fotos', $scope.foto) //Envia para o endereço o objeto do scope.
                 .success(function(){
                     $scope.foto = {};
+                    $scope.mensagem = "Foto adicionada com sucesso!";
                 }).error(function(){ //Caso dê errado!
-                    console.log("Erro!")
+                    $scope.mensagem = "Algum problema aconteceu! Tente mais tarde!";
                 });
         } else {
             console.log('Algo na requisição está errado!');
