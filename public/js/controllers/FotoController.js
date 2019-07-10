619bcb978b9554d3e653057f223f7e79f40ae610
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-07-09 22:57:24 -0300

    Incluindo post para novas fotos

--- public/js/controllers/FotoController.js
+++ public/js/controllers/FotoController.js
@@ -1,9 +1,20 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
 
+    //Controller associada a foto.html e receberá os dados da página e realizará o processamento necessário.
     $scope.foto = {};
 
+    //Método do formulário para enviar dados à API.
     $scope.submeter = function() {
-        console.log($scope.foto);
+        if($scope.formulario.$valid){
+            $http.post('v1/fotos', $scope.foto) //Envia para o endereço o objeto do scope.
+                .success(function(){
+                    $scope.foto = {};
+                }).error(function(){ //Caso dê errado!
+                    console.log("Erro!")
+                });
+        } else {
+            console.log('Algo na requisição está errado!');
+        };
     };
 
 });
