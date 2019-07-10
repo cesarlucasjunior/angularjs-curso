93904a53b43b3ab58a278fbbd40759ce4c0b5f20
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-07-09 22:39:16 -0300

    Inserindo validações - required e maxLength

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,13 @@
 angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
 .config(function($routeProvider, $locationProvider){
 
+    //main.js é o módulo principal da aplicação.
+
     $locationProvider.html5Mode(true);
 
     //Roteamento interno, ou seja, do angular pra gerenciarmos as views.
+
+    //Nesse módulo cadastramos o roteamento com o template e a controller responsável.
     $routeProvider.when('/fotos', {
         templateUrl : 'partials/principal.html',
         controller: 'FotosController'
