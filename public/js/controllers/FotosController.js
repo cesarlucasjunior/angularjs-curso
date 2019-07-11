b4c65ad8b0e16a7ce6b219252199ae26a5f286de
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-07-10 23:00:14 -0300

    Personalizando mensagem de deleção

--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -2,6 +2,8 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 
     $scope.fotos = [];
     $scope.filtro = ''; //Texto digitado pelo usuário - ng-model - two way data binding.
+    $scope.mensagem = '';
+
 
     $http.get('v1/fotos')
         .success(function(fotos){
