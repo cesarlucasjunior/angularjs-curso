f2c82b13bb6a1b43fb513e5cff82e2d7b012fb91
cesarlucasjunior <cesarlucasjunior93@gmail.com>
2019-07-10 22:55:56 -0300

    Adicionando funcao para deletar foto

--- public/js/controllers/FotoController.js
+++ public/js/controllers/FotoController.js
@@ -1,13 +1,13 @@
 angular.module('alurapic').controller('FotoController', function($scope, $http){
 
     //Controller associada a foto.html e receberá os dados da página e realizará o processamento necessário.
-    $scope.foto = {};
+    $scope.foto= {};
     $scope.mensagem = "";
 
     //Método do formulário para enviar dados à API.
     $scope.submeter = function() {
         if($scope.formulario.$valid){
-            $http.post('v1/fotos', $scope.foto) //Envia para o endereço o objeto do scope.
+            $http.post('/v1/fotos', $scope.foto) //Envia para o endereço o objeto do scope.
                 .success(function(){
                     $scope.foto = {};
                     $scope.mensagem = "Foto adicionada com sucesso!";
@@ -18,5 +18,4 @@ angular.module('alurapic').controller('FotoController', function($scope, $http){
             console.log('Algo na requisição está errado!');
         };
     };
-
 });
--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -12,6 +12,27 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
             console.log(error);
         })
 
+    $scope.remover = function(foto){
+        console.log('chamado');
+        $http.delete('/v1/fotos/' + foto._id)
+        .success(function(){
+            console.log('Deletado com sucesso!');
+            $scope.mensagem = "Foto " + foto.descricao + " deletada com sucesso!";
+            //Recarregar lista de fotos no cliente:
+            var indice = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indice, 1);
+        }).error(function(erro){
+            console.log('Erro na deleção - ' + erro);
+            $scope.mensagem = "Erro na deleção!";
+        });
+    };
+
+
+
+
+    
+
+
 
 
 
